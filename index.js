66c77853bacb6292a725b34d7b4351ce3d069b09
niftylettuce <niftylettuce@gmail.com>
2020-05-09 20:14:58 -0500

    fix: removed VALUE in messages due to locales pollution

--- index.js
+++ index.js
@@ -4,23 +4,22 @@ const errors = {
   general: {
     default: '{PATH} was invalid; please contact us if necessary.',
     required: '{PATH} is required.',
-    unique: '{PATH} of "{VALUE}" is not unique.'
+    unique: '{PATH} is not unique.'
   },
   Number: {
-    min: '{PATH} of ({VALUE}) is less than minimum allowed value of ({MIN}).',
-    max: '{PATH} of ({VALUE}) is more than maximum allowed value ({MAX}).'
+    min: '{PATH} is less than minimum allowed value of ({MIN}).',
+    max: '{PATH} is more than maximum allowed value ({MAX}).'
   },
   Date: {
-    min: '{PATH} of ({VALUE}) is before minimum allowed value ({MIN}).',
-    max: '{PATH} of ({VALUE}) is after maximum allowed value ({MAX}).'
+    min: '{PATH} is before minimum allowed value ({MIN}).',
+    max: '{PATH} is after maximum allowed value ({MAX}).'
   },
   String: {
-    enum: '{PATH} has an invalid selection of "{VALUE}".',
-    match: '{PATH} has an invalid value "{VALUE}".',
+    enum: '{PATH} has an invalid selection.',
+    match: '{PATH} has an invalid value.',
     minlength:
-      '{PATH} of "{VALUE}" is shorter than the minimum allowed length ({MINLENGTH}).',
-    maxlength:
-      '{PATH} of "{VALUE}" is longer than the maximum allowed length ({MAXLENGTH}).'
+      '{PATH} is shorter than the minimum allowed length ({MINLENGTH}).',
+    maxlength: '{PATH} is longer than the maximum allowed length ({MAXLENGTH}).'
   }
 };
 
